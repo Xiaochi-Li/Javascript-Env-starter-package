9fb7c622262f07000bdeacf0b65499d531621330
sean.li <sean.li@ladbrokes.com.au>
2018-10-15 15:51:25 +1000

    set up mock server

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,7 +4,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-var port = 3000;
+var port = 4000;
 var app = express();
 const compiler =webpack(config);
 
@@ -16,6 +16,13 @@ app.use(require('webpack-dev-middleware')(compiler,{
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
+
+app.get('/users', function(req,res){
+  res.json([
+    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id":2,"firstName":"Pop","lastName":"Ahaha","email":"pop@gmail.com"},
+  ]);
+});
 
 app.listen(port, function (err) {
   if (err) {
